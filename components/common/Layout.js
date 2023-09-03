dfb22edd802e51f72d24594910c6b4a81a52c8ee
Vu Nguyen <ngtrvu@gmail.com>
2023-09-03 23:13:37 +0700

    Add metadata functions

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,13 +1,9 @@
 'use client'
 
-const Layout = ({ isHome = false, children, settings, hasCover = true }) => {
+const Layout = ({ children, settings }) => {
   if (!settings) {
     return 'settings error'
   }
-
-  let classNames = ''
-  if (isHome) classNames = `${classNames} home-template`
-  if (hasCover) classNames = `${classNames} has-sans-body has-cover`
 
   return <div className="site-content">{children}</div>
 }
